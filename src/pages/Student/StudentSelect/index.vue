3b7d05eb8275942a453c66cbdc4e05512ce8b478
SunYong <924466726@qq.com>
2022-02-25 14:08:59 +0800

    .

--- src/pages/Student/StudentSelect/index.vue
+++ src/pages/Student/StudentSelect/index.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="teacherdata">
-
-
     <el-form :inline="true" :model="formInline" class="formselect">
       <el-form-item label="姓名">
         <el-input v-model="formInline.name" placeholder="姓名"></el-input>
@@ -15,11 +13,20 @@
       </el-form-item>
       <el-form-item label="院系名称">
         <el-select v-model="formInline.college" placeholder="院系名称">
-          <el-option label="计算机工程与科学学院" value="计算机工程与科学学院"></el-option>
+          <el-option
+            label="计算机工程与科学学院"
+            value="计算机工程与科学学院"
+          ></el-option>
           <el-option label="理学院" value="理学院"></el-option>
           <el-option label="钱伟长学院" value="钱伟长学院"></el-option>
-          <el-option label="材料科学与工程学院" value="材料科学与工程学院"></el-option>
-          <el-option label="通信与信息工程学院" value="通信与信息工程学院"></el-option>
+          <el-option
+            label="材料科学与工程学院"
+            value="材料科学与工程学院"
+          ></el-option>
+          <el-option
+            label="通信与信息工程学院"
+            value="通信与信息工程学院"
+          ></el-option>
           <el-option label="经济学院" value="经济学院"></el-option>
           <el-option label="管理学院" value="管理学院"></el-option>
           <el-option label="文学院" value="文学院"></el-option>
@@ -27,11 +34,12 @@
         </el-select>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">查询</el-button>
-        <el-button type="primary" @click="prevPage">上一页</el-button>
-        <el-button type="primary" @click="nextPage">下一页</el-button>
+        <el-button type="primary" @click="conditionQuery">查询</el-button>
+        <el-button type="primary" @click="prevPage" v-show="pageIndex == 1 ">上一页</el-button>
+        <el-button type="primary" @click="nextPage" >下一页</el-button>
       </el-form-item>
     </el-form>
+
 
 
     <el-table :data="TeacherList.slice(0,50)" border style="width: 100%">
@@ -41,12 +49,17 @@
       <el-table-column prop="title" label="职称" width="100"> </el-table-column>
       <el-table-column prop="college" label="院系名称" width="200"> </el-table-column>
 
+
       <el-table-column prop="telephone" label="手机号"> </el-table-column>
       <el-table-column prop="email" label="邮箱"> </el-table-column>
-      <el-table-column prop="website" label="个人网站" width="180"> </el-table-column>
-      <el-table-column prop="field" label="科研方向" width="180"> </el-table-column>
-      <el-table-column prop="num" label="已选择人数" width="180"> </el-table-column>
+      <el-table-column prop="website" label="个人网站" width="180">
+      </el-table-column>
+      <el-table-column prop="field" label="科研方向" width="180">
+      </el-table-column>
+      <el-table-column prop="num" label="已选择人数" width="180">
+      </el-table-column>
       <el-table-column fixed="right" label="操作" width="150">
+
       <template>
         <el-popconfirm
           title="确定要申请吗？"
@@ -55,6 +68,9 @@
         </el-popconfirm>
       </template>
     </el-table-column>
+
+
+
     </el-table>
 
     <el-pagination
@@ -73,29 +89,54 @@ import { mapState, mapGetters } from "vuex";
 export default {
   data() {
     return {
-      pageIndex:0,
+      pageIndex: 1,
       formInline: {
-        name: '',
-        title: '',
-        college: '',
-        }
+        name: "",
+        title: "",
+        college: "",
+      },
     };
   },
   computed: {
     ...mapGetters(["TeacherList"]),
   },
   methods: {
-    onSubmit() {
-      console.log('submit!');
+    conditionQuery() {
+      this.pageIndex = 1;
+      this.$store.dispatch("getTeacherList", {
+        keyword: {
+          name: this.formInline.name || null,
+          college: this.formInline.college || null,
+          title: this.formInline.title || null,
+        },
+        page: 1,
+        size: 50,
+      });
+      console.log("query!");
     },
-    prevPage(){
+    prevPage() {
       this.pageIndex-=1;
-      if(pageIndex<0) pageIndex=0;
+      this.$store.dispatch("getTeacherList", {
+        keyword: {
+          name: this.formInline.name || null,
+          college: this.formInline.college || null,
+          title: this.formInline.title || null,
+        },
+        page: this.pageIndex,
+      });
     },
-    nextPage(){
+    nextPage() {
       this.pageIndex+=1;
-    }
-  }
+      this.$store.dispatch("getTeacherList", {
+        keyword: {
+          name: this.formInline.name || null,
+          college: this.formInline.college || null,
+          title: this.formInline.title || null,
+        },
+        page: this.pageIndex,
+      });
+    },
+  },
 };
 </script>
 
