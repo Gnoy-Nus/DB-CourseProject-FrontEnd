5625c103c68bdca18ac8f915204b9696cb852656
SunYong <924466726@qq.com>
2022-02-28 11:11:49 +0800

    .

--- vue.config.js
+++ vue.config.js
@@ -7,10 +7,22 @@ proxyObj['/'] = {
     }
 }
 module.exports = {
+    publicPath:"./",
     lintOnSave: false,
     devServer: {
         host: 'localhost',
         port: 8080,
         proxy: proxyObj
+    },
+    configureWebpack: config => {
+        // 为生产环境修改配置...
+        if (process.env.NODE_ENV === 'production') {
+            config.mode = 'production';
+            // 打包文件大小配置
+            config.performance = {
+              maxEntrypointSize: 10000000,
+              maxAssetSize: 30000000
+            }
+        }
     }
 }
